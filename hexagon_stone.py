851d98bdf5ea15efea67268dcfa13599c8912917
donnyhai <tillmannbosch2@gmail.com>
2019-08-31 02:53:20 +0200

    [T] 190831
    new stone structure, input surface

--- hexagon_stone.py
+++ hexagon_stone.py
@@ -3,15 +3,16 @@ from math import sqrt
 
 pygame.init()
 
-class hexagon_stone:
+class Hexagon_Stone:
     
-    def __init__(self, size, position, surface, stone):
+    def __init__(self, size, surface, stone):
         self.size = size
-        self.position = position
         self.surface = surface
         self.stone = stone
+        
+    def set_position(self, coord):
+        self.position = coord
            
-    
     def getting_hexa(scaling_ratio, start_vector):    
         hex_coords = [(0,0), (1,0), (1.5, 3**(1/2)/2), (1, 3**(1/2)), (0,3**(1/2)), (-0.5, 3**(1/2)/2)]
         scaled_coords = []
--- player.py
+++ player.py
@@ -1,22 +1,26 @@
+import hexagon_stone as hs
 import stone as s
 
 class Player:
-    def __init__(self, color):
+    def __init__(self, color, surface):
         self.color = color
-        self.stones = self.create_stones()
-    def create_stones(self):
-        stones = {"bee": s.Stone("bee", 1),
-                "ant1": s.Stone("ant",1),
-                "ant2": s.Stone("ant",2),
-                "ant3": s.Stone("ant",3),
-                "hopper1": s.Stone("hopper", 1),
-                "hopper2": s.Stone("hopper", 2),
-                "hopper3": s.Stone("hopper", 3),
-                "spider1": s.Stone("spider", 1),
-                "spider2": s.Stone("spider", 2)}
-        for stone in stones.values():
-            stone.set_color(self.color)
-        return stones
+        self.surface = surface
+        self.stone_size = 0 ####################################edit, example: 0.05 * surface
+        self.stones = self.create_stones(self.stone_size)
+        
+    def create_stones(self, stone_size):
+        hstones = {"bee": hs.Hexagon_Stone(stone_size, self.surface, s.Stone("bee", 1)),
+                "ant1": hs.Hexagon_Stone(stone_size, self.surface, s.Stone("ant",1)),
+                "ant2": hs.Hexagon_Stone(stone_size, self.surface, s.Stone("ant",2)),
+                "ant3": hs.Hexagon_Stone(stone_size, self.surface, s.Stone("ant",3)),
+                "hopper1": hs.Hexagon_Stone(stone_size, self.surface, s.Stone("hopper", 1)),
+                "hopper2": hs.Hexagon_Stone(stone_size, self.surface, s.Stone("hopper", 2)),
+                "hopper3": hs.Hexagon_Stone(stone_size, self.surface, s.Stone("hopper", 3)),
+                "spider1": hs.Hexagon_Stone(stone_size, self.surface, s.Stone("spider", 1)),
+                "spider2": hs.Hexagon_Stone(stone_size, self.surface, s.Stone("spider", 2))}
+        for hstone in hstones.values():
+            hstone.stone.set_color(self.color)
+        return hstones
     
 class Human_Player(Player):
     pass
