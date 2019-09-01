0ef7339b97d0a62c92499703a4545a3e80a85be5
Thranduilsohn <52779376+Thranduilsohn@users.noreply.github.com>
2019-09-01 17:32:40 +0200

    [S] 0109_finish

--- hexagon_stone.py
+++ hexagon_stone.py
@@ -26,10 +26,10 @@ class hexagon_stone:
         return hexagon_points[0]+((hexagon_points[1]-hexagon_points[0])*0.5, (hexagon_points[1]-hexagon_points[0])* 3**(0.5)*0.5)
 
     def hexa_stone_draw_frame(self, position):
-        pygame.draw.aalines(self.surface, self.color , True, self.getting_hexa(self.size, position), 2)
+        pygame.draw.aalines(self.surface, self.stone.color , True, self.getting_hexa(self.size, position), 2)
         
     def draw_stone(self, position):
-        pygame.draw.polygon(self.surface, self.color , self.getting_hexa(self.size, position))
+        pygame.draw.polygon(self.surface, self.stone.color , self.getting_hexa(self.size, position))
         
     def euclidean_metric(self, vector):
         squared = [x*x for x in vector]
