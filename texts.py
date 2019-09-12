561e8324c5acf427139e31b35d13c3d6a327952c
donnyhai <tillmannbosch2@gmail.com>
2019-09-12 02:39:37 +0200

    [T] added texts file

---
+++ texts.py
@@ -0,0 +1,11 @@
+welcome_text = "Welcome to this hive game! White begins."
+
+win_text_white = "White wins. Good game!"
+
+win_text_black = "Black wins. Nice game!"
+
+win_text_equal = "Tied! Interesting game!"
+
+bee_reminder = "See, that you lay your bee until your 4. move."
+
+computer_player_text = "I am a computer player! Lets see how I handle this..."
